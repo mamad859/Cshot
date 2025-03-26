fcfe918477a3838053470a410f39b1b734596520
Rasool Rajabi <rasool.top.85@gmail.com>
2025-03-26 14:06:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,34 @@ from TargetAndItems import *
 from pygameCshot import *
 
 def shooting(player):
-    if sable1.check_hit(player.x, player.y, 1):
+    if sable1.check_hit(player.x, player.y, 0):
         player.score = sable1.apply_effect(player.score)
-    if sable2.check_hit(player.x, player.y, 2):
+
+    if sable2.check_hit(player.x, player.y, 1):
         player.score = sable2.apply_effect(player.score)
-    if sable3.check_hit(player.x, player.y, 3):
+
+    if sable3.check_hit(player.x, player.y, 2):
         player.score = sable3.apply_effect(player.score)
-    if bomb1.check_hit(player.x, player.y, 4):
+
+    if bomb1.check_hit(player.x, player.y, 3):
         player.score = bomb1.apply_effect(player.score)
-    if speed.check_hit(player.x, player.y, 5):
+
+    if speed.check_hit(player.x, player.y, 4):
         player.speed = speed.apply_effect(player.speed)
-    if time1.check_hit(player.x, player.y, 6):
+
+    if time1.check_hit(player.x, player.y, 5):
         player.time = time1.apply_effect(player.time)
-    if bullet1.check_hit(player.x, player.y, 7):
+
+    if bullet1.check_hit(player.x, player.y, 6):
         player.bullet = bullet1.apply_effect(player.bullet)
-        
-def main_game(player1_name, player2_name):
+
+def main():
     pg.init()
     pg.mixer.init()
     pg.mixer.music.load("Cshot.mp3")
     pg.mixer.music.set_volume(0.5)
     pg.mixer.music.play(-1)
     
-    pg.init()
     screen = pg.display.set_mode((800, 600))
     pg.display.set_caption("Cshot")
     running = True
@@ -37,6 +42,7 @@ def main_game(player1_name, player2_name):
         player2.time -= 1/60
         if (player1.time < 0 and player2.time < 0) or (player1.bullet == 0 and player2.bullet == 0):
             running = False
+            break
         for event in pg.event.get():
             if event.type == QUIT:
                 running = False
@@ -68,14 +74,11 @@ def main_game(player1_name, player2_name):
                         shooting(player2)
                         points.append([player2.x, player2.y, 2])
                         player2.bullet -= 1
-                elif event.key == K_ESCAPE:
-                    running = False
-                    pg.quit()
 
         screen.fill(cyan)
 
-        display_time(screen, player1.time, 5, 60, purple)
-        display_time(screen, player2.time, 630, 60, purple)
+        display_time(screen, player1.time, player2.time)
+        display_score(screen, player1, player2)
 
         for point in points:
             if point[2] == 1:
@@ -90,16 +93,13 @@ def main_game(player1_name, player2_name):
         screen.blit(timer, (targets[5][0], targets[5][1]))
         screen.blit(bullet, (targets[6][0], targets[6][1]))
 
-        display_score(screen, player1, 5, 10)
-        display_score(screen, player2, 630, 10)
-
-        sable1 = Sable(1, 25)
-        sable2 = Sable(2, 25)
-        sable3 = Sable(3, 25)
-        bomb1 = Bomb(4, 50)
-        speed = Speed(5, 30)
-        time1 = Time(6, 30)
-        bullet1 = Bullet(7, 30)
+        sable1 = Sable(0, 25)
+        sable2 = Sable(1, 25)
+        sable3 = Sable(2, 25)
+        bomb1 = Bomb(3, 50)
+        speed = Speed(4, 30)
+        time1 = Time(5, 30)
+        bullet1 = Bullet(6, 30)
         pg.display.update()
         clock.tick(60)
 
@@ -110,20 +110,21 @@ def main_game(player1_name, player2_name):
     pg.quit()
     
 if __name__ == "__main__" :
-    sable1 = Sable(1, 25)
-    sable2 = Sable(2, 25)
-    sable3 = Sable(3, 25)
-    bomb1 = Bomb(4, 50)
-    speed = Speed(5, 25)
-    time1 = Time(6, 25)
-    bullet1 = Bullet(7, 30)
+
+    sable1 = Sable(0, 25)
+    sable2 = Sable(1, 25)
+    sable3 = Sable(2, 25)
+    bomb1 = Bomb(3, 50)
+    speed = Speed(4, 25)
+    time1 = Time(5, 25)
+    bullet1 = Bullet(6, 30)
 
     player1 = Player(random.randint(10, 790), random.randint(10, 590))
     player2 = Player(random.randint(10, 790), random.randint(10, 590))
-    player1_name, player2_name = "", ""
+    # player1_name,player2_name = aut.start_game()
+    player1_name, player2_name = "rasool", "mamad"
     player1.name = player1_name ; player2.name = player2_name
     points = []
     clock = pg.time.Clock()
 
-    player1_name,player2_name = aut.start_game()
-    main_game(player1_name, player2_name)
+    main()
